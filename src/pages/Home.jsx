1619b8a0164f6618213d549cc5123dc8fe70593b
Braian Gomez <braiandenisgomez@gmail.com>
2022-12-08 21:01:26 -0300

    Added clients static data Added loader function to return that clients data Added a table with the clients information

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,77 @@
+import { useLoaderData } from "react-router-dom";
+import Client from "../components/Client";
+
+export function loader() {
+  const clients = [
+    {
+      id: 1,
+      name: "Juan",
+      phone: 102013313,
+      email: "juan@juan.com",
+      company: "Codigo Con Juan",
+    },
+    {
+      id: 2,
+      name: "Karen",
+      phone: 138198313,
+      email: "karen@juan.com",
+      company: "Codigo Con Juan",
+    },
+    {
+      id: 3,
+      name: "Josue",
+      phone: 31983913,
+      email: "josue@juan.com",
+      company: "Codigo Con Juan",
+    },
+    {
+      id: 4,
+      name: "Miguel",
+      phone: 319381983,
+      email: "miguel@juan.com",
+      company: "Codigo Con Juan",
+    },
+    {
+      id: 5,
+      name: "Pedro",
+      phone: 1398198938,
+      email: "pedro@juan.com",
+      company: "Codigo Con Juan",
+    },
+  ];
+
+  return clients;
+}
+
 const Home = () => {
-  return <div>asdasd</div>;
+  const clients = useLoaderData();
+
+  return (
+    <>
+      <h1 className="font-black text-4xl text-blue-400">Clients</h1>
+      <p className="mt-2">Manage your clients</p>
+
+      {clients.length ? (
+        <table className="w-full bg-white shadow mt-5 table-auto">
+          <thead className="bg-blue-800 text-white">
+            <tr>
+              <th className="p-2">Clients</th>
+              <th className="p-2">Contact</th>
+              <th className="p-2">Actions</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {clients.map((client) => (
+              <Client client={client} key={client.id} />
+            ))}
+          </tbody>
+        </table>
+      ) : (
+        <p className="text-center mt-10">There is no clients yet.</p>
+      )}
+    </>
+  );
 };
 
 export default Home;
