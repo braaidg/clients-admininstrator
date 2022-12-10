558d8bb80afc95cdc54842b0deac9398f6a09ae1
Braian Gomez <braiandenisgomez@gmail.com>
2022-12-09 21:15:15 -0300

    Added get client action

--- src/data/Clients.js
+++ src/data/Clients.js
@@ -7,6 +7,15 @@ export async function getClients() {
   return results;
 }
 
+export async function getClient(id) {
+  const url = import.meta.env.VITE_API_URL;
+
+  const response = await fetch(`${import.meta.env.VITE_API_URL}/${id}`);
+  const results = await response.json();
+
+  return results;
+}
+
 export async function addClient(data) {
   try {
     const response = await fetch(import.meta.env.VITE_API_URL, {
